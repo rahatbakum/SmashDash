124be579ad6a61cb1fd19134706518f638686b7c
rahatbakum <vovnedj@gmail.com>
2022-03-28 16:48:53 +0300

    Mass Applier is working now

--- Assets/Scripts/ForTesting/TestMassApplier.cs
+++ Assets/Scripts/ForTesting/TestMassApplier.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class TestMassApplier : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private MassApplier _massApplier;
     void Start()
     {
-        
+        _massApplier = GetComponent<MassApplier>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButton(0))
+        {
+            float mass = 2f * Mathf.Clamp((float) Input.mousePosition.x / Screen.width, 0f, 1f);
+            Debug.Log($"ApplyMass({mass})");
+            _massApplier.ApplyMass(mass);
+        }
     }
 }
--- Assets/Scripts/MassApplier.cs
+++ Assets/Scripts/MassApplier.cs
@@ -4,31 +4,34 @@ using UnityEngine;
 public class MassApplier : MonoBehaviour
 {
     private SphereCollider _sphereCollider;
-    private float _startRadius;
+    private float _startColliderRadius;
+    private Vector3 _startScale;
     
     private void Start()
     {
         _sphereCollider = GetComponent<SphereCollider>();
-        _startRadius = _sphereCollider.radius;
+        _startColliderRadius = _sphereCollider.radius;
+        _startScale = transform.localScale;
     } 
 
     private float MassToRadius(float mass) => Mathf.Sqrt(mass);
 
     private float RadiusToMass(float radius) => Mathf.Pow(radius, 2f);
 
-    private void ApplyRadius(float radius)
+    private void ApplyCollider(float radius)
     {
-        _sphereCollider.radius = radius;
+        _sphereCollider.radius = radius * _startColliderRadius;
     }
 
-    private void ApplyPosition(float radius)
+    private void ApplyTransform(float radius)
     {
-        transform.position = new Vector3(transform.position.x, radius, transform.position.z);
+        transform.localScale = radius * _startScale;
+        transform.position = new Vector3(transform.position.x, radius * _startColliderRadius * _startScale.y, transform.position.z);
     }
     public void ApplyMass(float mass)
     {
         float radius = MassToRadius(mass);
-        ApplyRadius(radius);
-        ApplyPosition(radius);
+        ApplyCollider(radius);
+        ApplyTransform(radius);
     }
 }
